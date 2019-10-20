2df36a9be09951dd65845da1a5d0e16fe45987a3
Andreas Koj <kojandreas@gmail.com>
2019-10-20 21:39:44 +0800

    implement message switching, polish ui

--- app.js
+++ app.js
@@ -2,88 +2,120 @@ var express = require('express');
 var app = express();
 var monk = require('monk');
 var db = monk('localhost:27017/data');
+var session = require('express-session')
 
 var server = app.listen(8081, function () {
-
   var host = server.address().address;
   var port = server.address().port;
   console.log("Starting webMail app at http://localhost%s%s", host, port);
 })
 
+//set up session
+app.use(session({
+    secret: 'HpSVPhmTFAtpNb0oIrBO',
+    resave: true,
+    saveUninitialized: true
+})) 
+
 //set default page to webmail.html, initalize database
 app.use(express.static('public', {index: 'webmail.html'}), function(req,res,next){
     req.db = db;
-
     next();
 })
 
-app.get('/inbox', function(req, res){
+// display mailboxes
+app.get('/retriveemaillist', function(req, res){
     var db = req.db;
     var collection = db.get('emailList');
-
-    collection.find({mailbox: 'Inbox'}, {}, function(err, docs){
+    var type = req.query.type;
+       
+    collection.find({mailbox: type}, {}, function(err, docs){
         if (err === null){
-            res.send(emailResponseHTML(docs));
+            
+            //create list of ids and store it in session 
+            req.session.mailList = [];
+            for(var i=0; i<docs.length; i++) {
+                req.session.mailList.push(docs[i]["_id"]);
+            }
+            res.send(emailListResponseHTML(docs));
         } else console.log(err);
     });
-    
 });
 
-app.get('/important', function(req, res){
+//display email content and pass data about next and previous email
+app.get('/getmail', function(req, res){
     var db = req.db;
     var collection = db.get('emailList');
-
-    collection.find({mailbox: 'Important'}, {}, function(err, docs){
+    var id = req.query.id;
+        
+    collection.find({_id: id }, {}, function(err, docs){
         if (err === null){
-            res.send(emailResponseHTML(docs));
+            var mailList = req.session.mailList;
+            res.send(emailContentResponseHTML(docs, prepareIDs(mailList, id)));
         } else console.log(err);
     });
-    
 });
 
-app.get('/sent', function(req, res){
-    var db = req.db;
-    var collection = db.get('emailList');
+// function takes mailList from session an current ID to find next and previous ids
+function prepareIDs(mailList, currentID) {
+    var currentPostionID = mailList.indexOf(currentID);
+    var prevID;
+    var nextID;
 
-    collection.find({mailbox: 'Sent'}, {}, function(err, docs){
-        if (err === null){
-            res.send(emailResponseHTML(docs));
-        } else console.log(err);
-    });
-    
-});
+    if(currentID == 0) {
+        nextID = mailList[currentPostionID+1];
+        prevID = undefined;
+    }
+    else if(currentID == mailList.length-1) {
+        prevID = mailList[currentPostionID-1];
+        nextID = undefined;
+    }
+    else {
+        prevID = mailList[currentPostionID-1];
+        nextID = mailList[currentPostionID+1];
+    }
+    return [prevID,nextID];
+}
 
-app.get('/trash', function(req, res){
-    var db = req.db;
-    var collection = db.get('emailList');
+// create view for specific email
+function emailContentResponseHTML(data, ids){
+    var responseString = "";
+    responseString += "<div id='mailContent'>"
+    responseString += "<p>"+data[0]['title']+"</p>";
+    responseString += "<p>"+data[0]['time']+"</p>";
+    responseString += "<p>"+data[0]['sender']+"</p>";
+    responseString += "<p>"+data[0]['recipient']+"</p>";
+    responseString += "<p>"+data[0]['content']+"</p>";
+    responseString += "<p id='prevID'>"+ids[0]+"</p>";    
+    responseString += "<p id='nextID'>"+ids[1]+"</p>";    
+    responseString += "</div>";
+    return responseString;      
+}; 
 
-    collection.find({mailbox: 'Trash'}, {}, function(err, docs){
-        if (err === null){
-            res.send(emailResponseHTML(docs));
-        } else console.log(err);
-    });
-    
-});
-
-function emailResponseHTML(data) {
+// create list of emails view
+function emailListResponseHTML(data) {
     
     var responseString ="<ul id='mailList'>";
     
     responseString += "<li><span>Recipient</span><span>Title</span><span>Time</span></li>"
     
     for (var i = 0; i < data.length; i++) {
+        
         var email = data[i];
-        responseString += "<li><input type='checkbox'><span>";
+        var emaiID = '"';
+        emaiID += email["_id"];
+        emaiID += '"';
+        
+        responseString += "<li><input type='checkbox'><div class='email-line' onclick='openMail(event,"+emaiID+")'><span>";
         responseString += email['recipient'];
         responseString +="</span><span>"
         responseString += email['title'];
         responseString +="</span><span>"
         responseString += email['time'];
-        responseString += "</span>";
-        responseString += "</li>"
+        responseString +="</span></div>";
+        responseString += "</li>";
+        
     }
-    
     responseString += "</ul>";
-    
     return responseString;
-}
+};
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,6 @@
 // init self invoking function which fetches inbox emails
 (function () {
-    webMailHttpRequest("inbox");
+    webMailHttpRequest("retriveemaillist?type=Inbox");
 })();
 
 function inboxButton(ev){
@@ -8,48 +8,92 @@ function inboxButton(ev){
     $("#mailbox>div").removeClass("selected");
     //adds class selected to this id
     $("#"+ev.target.id).addClass("selected");
-    // remove previous content
+    // remove mail list 
     $("#mailList").remove();
+    // remove mail content
+    $("#mailContent").remove();
     // show loading circle
     $("#loading").show(); 
     
-    webMailHttpRequest("inbox");
+    webMailHttpRequest("retriveemaillist?type=Inbox");
 };
 
 function importantButton(ev){
     $("#mailbox>div").removeClass("selected");
     $("#"+ev.target.id).addClass("selected");
     $("#mailList").remove();
+    $("#mailContent").remove();
     $("#loading").show(); 
     
-    webMailHttpRequest("important");
+    webMailHttpRequest("retriveemaillist?type=Important");
 };
 
 function sentButton(ev){
     $("#mailbox>div").removeClass("selected");
     $("#"+ev.target.id).addClass("selected");
     $("#mailList").remove();
+    $("#mailContent").remove();
     $("#loading").show(); 
     
-    webMailHttpRequest("sent");
+    webMailHttpRequest("retriveemaillist?type=Sent");
 };
 
 function trashButton(ev){
     $("#mailbox>div").removeClass("selected");
     $("#"+ev.target.id).addClass("selected");
     $("#mailList").remove();
+    $("#mailContent").remove();
     $("#loading").show(); 
     
-    webMailHttpRequest("trash");
+    webMailHttpRequest("retriveemaillist?type=Trash");
 };
 
+function moveToButton(ev) {
+    console.log("clicked!");
+}
+
+//logic for previous button 
+function previousButton(ev) {
+    // check if the mail details view is on the screen
+    if($("#mailContent").length){
+        var prev =$("#prevID").html(); 
+        
+        if (prev != "undefined"){
+            openMail(ev,prev);
+        }    
+    }
     
+    else {
+        // implement witching pages of emails
+        console.log("mail view list")
+    }
+};
+
+//logic for next button
+function nextButton(ev) {
+    if($("#mailContent").length){
+        var next = $("#nextID").html(); 
+    
+        if (next != "undefined"){
+            openMail(ev,next);
+        }           
+    }
+    
+    else{
+        // implement witching pages of emails
+        console.log("mail view list")
+    }  
+};
+
+function openMail(ev, emailID) {
+    $("#mailList").remove();
+    $("#mailContent").remove();
+    webMailHttpRequest("getmail?id="+emailID);
+};
+ 
 // reqType - type of request
 function webMailHttpRequest(reqType) {
-    
-    console.log("init wmhr");
     var xmlhttp = new XMLHttpRequest();
-    
     xmlhttp.open("GET",reqType, true);
     xmlhttp.send();
 
