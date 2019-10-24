ee0365d0660ec997b564567c85b6c44dbe0ecff7
Andreas Koj <kojandreas@gmail.com>
2019-10-24 20:24:20 +0800

    implement switching pages

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var db = monk('localhost:27017/data');
 var session = require('express-session')
 
 // constants
-var EMAILS_ON_PAGE = 2;
+var EMAILS_ON_PAGE = 3;
 
 var server = app.listen(8081, function () {
   var host = server.address().address;
@@ -24,7 +24,7 @@ app.use(session({
 app.use(express.static('public', {index: 'webmail.html'}), function(req,res,next){
     req.db = db;
     next();
-})
+})  
 
 // display list of emails
 app.get('/retriveemaillist', function(req, res){
@@ -32,25 +32,17 @@ app.get('/retriveemaillist', function(req, res){
     var collection = db.get('emailList');
     var type = req.query.type;
     var pageNo = parseInt(req.query.page);
-    console.log(pageNo);
+//    console.log(pageNo);
        
     collection.find({mailbox: type}, {}, function(err, docs){
         if (err === null){
             
             // slice to get only one page 
             var page = docs.slice(EMAILS_ON_PAGE*(pageNo-1),EMAILS_ON_PAGE*pageNo)
+            var pagesQty = Math.ceil(docs.length / EMAILS_ON_PAGE);
             
-            console.log(page);
+            res.send({response: page, pagesQty: pagesQty});
             
-            //create list of ids and store it in session 
-//            req.session.mailList = [];
-//            for(var i=0; i<docs.length; i++) {
-//                req.session.mailList.push(docs[i]["_id"]);
-//            }
-            
-            var emailListResponse = emailListResponseHTML(page);
-//            res.send({response: emailListResponse, emailListID: "test"});
-            res.send(emailListResponse);
         } else console.log(err);
     });
 });
@@ -107,30 +99,3 @@ function emailContentResponseHTML(data, ids){
     return responseString;      
 }; 
 
-// create list of emails view
-function emailListResponseHTML(data) {
-    
-    var responseString ="<ul id='mailList' class='box'>";
-    
-    responseString += "<li><span>Recipient</span><span>Title</span><span>Time</span></li>"
-    
-    for (var i = 0; i < data.length; i++) {
-        
-        var email = data[i];
-        var emaiID = '"';
-        emaiID += email["_id"];
-        emaiID += '"';
-        
-        responseString += "<li><input type='checkbox'><div class='email-line' onclick='openMail(event,"+emaiID+")'><span>";
-        responseString += email['recipient'];
-        responseString +="</span><span>"
-        responseString += email['title'];
-        responseString +="</span><span>"
-        responseString += email['time'];
-        responseString +="</span></div>";
-        responseString += "</li>";
-        
-    }
-    responseString += "</ul>";
-    return responseString;
-};
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,8 @@
 // global list with email IDs from current scope
-var emailListID = [];
-var currentPage= 2;
+var emailList = [];
+var currentPage = 1;
+var pagesQty;
+var currentMailbox = "";
 var composeHTML = 
     "<div class='box'><h3>Compose a new email</h3> To:<textarea></textarea> <br>Subject:<textarea></textarea><br> <textarea></textarea><br><button>Send</button></div>"
 
@@ -10,32 +12,43 @@ function openMail(ev, emailID) {
     webMailHttpRequest("getmail?id="+emailID);
 };  
 
-// reqType - type of request
-function webMailHttpRequest(reqType) {
-    var xmlhttp = new XMLHttpRequest();
-    
-//    console.log(reqType);
+//// reqType - type of request
+//function webMailHttpRequest(reqType) {
+//    var xmlhttp = new XMLHttpRequest();
 //    
-    xmlhttp.open("GET",reqType, true);
-    xmlhttp.send();
-
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-            $("#loading").hide(); 
-            
+//    xmlhttp.open("GET",reqType, true);
+//    xmlhttp.send();
+//
+//    xmlhttp.onreadystatechange = function() {
+//        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+//            $("#loading").hide(); 
 //            var responseJSON = JSON.parse(xmlhttp.response);
-            
-//            console.log(responseJSON["emailListID"]);
-            
-            $("#mail").append(xmlhttp.responseText);
-//            global = xmlhttp.responseText;
-        }
-    }
-}
+//            
+//            console.log("tesst");
+//            console.log(responseJSON);
+//            // parsing to JSON format 
+//            
+////            console.log(responseJSON["emailListID"]);
+//            
+////            $("#mail").append(xmlhttp.responseText);
+//            $("#mail").append(emailListResponseHTML(responseJSON));
+////            global = xmlhttp.responseText;
+//        }
+//    }
+//}
 
 $(document).ready(function(){
-
-    webMailHttpRequest("retriveemaillist?type=Inbox&page=" +currentPage);
+    retriveEmailList("Inbox");
+    
+    
+// intially tigger inboxButton to open inbox
+//    $("#inboxButton").trigger("click");
+//    $.get("retriveemaillist?type=Inbox&page=" +currentPage, function(data){
+//        $("#loading").hide();         
+//        $("#mail").append(emailListResponseHTML(data));
+//    });
+    
+//    webMailHttpRequest("retriveemaillist?type=Inbox&page=" +currentPage);
 
     $("#composeButton").click(function(){
         
@@ -49,104 +62,117 @@ $(document).ready(function(){
         console.log("test moveTo");  
         console.log(global);
     });    
-
-//    $(".email-line").click(function(){
-//        $(".box").remove();
-//        
-//        
-//        webMailHttpRequest("getmail?id="+emailID);
-//    });
     
     $("#inboxButton").click(function(){
+        currentPage=1;
         $("#mailbox>div").removeClass("selected");
         $(this).addClass("selected");
         $(".box").remove();
         $("#loading").show(); 
 
-        webMailHttpRequest("retriveemaillist?type=Inbox");
+        retriveEmailList("Inbox");
     });   
 
     $("#importantButton").click(function(){
-
+        currentPage=1;
         $("#mailbox>div").removeClass("selected");
         $(this).addClass("selected");
         $(".box").remove();
-//        $("#mailContent").remove();
         $("#loading").show(); 
 
-        webMailHttpRequest("retriveemaillist?type=Important");   
+        retriveEmailList("Important");
     });    
 
     $("#sentButton").click(function(){
-
+        currentPage=1;
         $("#mailbox>div").removeClass("selected");
         $(this).addClass("selected");
         $(".box").remove();
-//        $("#mailContent").remove();
         $("#loading").show(); 
 
-        webMailHttpRequest("retriveemaillist?type=Sent");  
+        retriveEmailList("Sent");
     });     
 
     $("#trashButton").click(function(){
-
+        currentPage=1;
         $("#mailbox>div").removeClass("selected");
         $(this).addClass("selected");
         $(".box").remove();
-//        $("#mailContent").remove();
         $("#loading").show(); 
-
-        webMailHttpRequest("retriveemaillist?type=Trash");   
+        
+        retriveEmailList("Trash");
     }); 
 
+    //previous button implementation
     $("#previousButton").click(function(){
         
         if($("#mailContent").length){
-            console.log("mail ")
-
+            console.log("mail");
         }
 
         else {
-            // implement witching pages of emails
-            console.log("mail list")
+            if(currentPage == 1){
+                return;
+            }
+            currentPage --;
+            $(".box").remove();
+            retriveEmailList(currentMailbox);
         }
-         
     });
-
-
-
-//    //logic for previous button 
-//    function previousButton(ev) {
-//        // check if the mail details view is on the screen
-//        if($("#mailContent").length){
-//            var prev =$("#prevID").html(); 
-//
-//            if (prev != "undefined"){
-//                openMail(ev,prev);
-//            }    
-//        }
-//
-//        else {
-//            // implement witching pages of emails
-//            console.log("mail view list")
-//        }
-//    };
-
-    //logic for next button
-    function nextButton(ev) {
+    
+    $("#nextButton").click(function(){
         if($("#mailContent").length){
-            var next = $("#nextID").html(); 
-
-            if (next != "undefined"){
-                openMail(ev,next);
-            }           
+            console.log("mail")
         }
 
         else{
-            // implement witching pages of emails
-            console.log("mail view list")
+            if(currentPage == pagesQty){
+                return;
+            }
+            // increment page number 
+            currentPage ++;
+            $(".box").remove();
+            retriveEmailList(currentMailbox);
         }  
+    });
+    
+    // create list of emails view
+    function emailListResponseHTML(data) {
+
+        var responseString ="<ul id='mailList' class='box'>";
+
+        responseString += "<li><span>Recipient</span><span>Title</span><span>Time</span></li>"
+
+        for (var i = 0; i < data.length; i++) {
+
+            var email = data[i];
+            var emaiID = '"';
+            emaiID += email["_id"];
+            emaiID += '"';
+
+            responseString += "<li><input type='checkbox'><div class='email-line' onclick='openMail(event,"+emaiID+")'><span>";
+            responseString += email['recipient'];
+            responseString +="</span><span>"
+            responseString += email['title'];
+            responseString +="</span><span>"
+            responseString += email['time'];
+            responseString +="</span></div>";
+            responseString += "</li>";
+
+        }
+        responseString += "</ul>";
+        return responseString;
+    };    
+
+    function retriveEmailList(type){
+        currentMailbox = type;
+        $.get("retriveemaillist?type="+type+"&page=" +currentPage, function(data){
+            $("#loading").hide();
+            emailList = data;
+            pagesQty = data["pagesQty"]; $("#mail").append(emailListResponseHTML(data["response"]));
+        });
     };
 
-
+    
 });
+
